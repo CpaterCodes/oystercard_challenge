4917ecd4b17d4731ba887a5a992fa22ad126a398
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-11-18 15:53:42 +0000

    Raises errorwhen top up threatens to push balance over £90

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,19 @@
 class OysterCard
 
 attr_reader :balance
-
+LIMIT = 90
   def initialize(balance = 0)
     @balance = balance
   end
 
   def top_up(amount)
+    raise "£90 balance maximum exceeded, transfer cancelled" if self.over_limit?(amount)
     @balance += amount
+  end
+
+
+  def over_limit?(money_added)
+    (@balance + money_added) > LIMIT
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,11 +7,17 @@ describe OysterCard do
     end
   end
 
-  describe "#top_up" do 
+  describe "#top_up" do
    it "should add given amount to balance" do
     subject.top_up(10)
     expect(subject.balance).to eq(10)
    end
-  end 
+
+   it "Should not push balance above £90" do
+     subject.top_up(10)
+     message = "£90 balance maximum exceeded, transfer cancelled"
+     expect(subject.top_up(90)).to raise_error message
+   end
+  end
 
 end
