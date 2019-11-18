08bd33ecf9ca23f4bea8057a4b6cf4fdd6fa4f7a
bensheridanedwards <bensheridanedwards@gmail.com>
2019-11-18 15:17:35 +0000

    Add top_up feature tests and functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,12 @@ describe OysterCard do
       expect(subject.balance).to eq(0)
     end
   end
+
+  describe "#top_up" do 
+   it "should add given amount to balance" do
+    subject.top_up(10)
+    expect(subject.balance).to eq(10)
+   end
+  end 
+
 end
