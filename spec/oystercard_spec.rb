d72a61aab0cc73e53ae3d40ccbfdb5406619622b
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-11-18 16:29:25 +0000

    resolving merge conflict

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -18,20 +18,8 @@ describe OysterCard do
      message = "£90 balance maximum exceeded. Balance: #{subject.balance}"
      expect{subject.top_up 1 }.to raise_error message
    end
-  end
 
-  describe "#deduct" do 
-    it "should deduct a specified fare from the users balance, then return the remaining balance" do 
-      subject.top_up(30)
-      expect(subject.deduct(10)).to eq(20)
-    end
-  end
 
-  describe "#touch_in" do 
-    it "should change the in_journey status to true" do 
-      subject.touch_in
-      expect(subject.in_journey).to be_truthy
-    end
   end
 
 end
